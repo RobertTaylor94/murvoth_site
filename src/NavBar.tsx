393f2379e117309269a1bf17906f91f87806b066
Robert Taylor <rjt903@gmail.com>
2025-02-17 21:02:21 +0000

    added dark mode toggle

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,6 +1,21 @@
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Container, Navbar, Nav, Form } from "react-bootstrap";
 
 function NavBar() {
+
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem('darkMode') === 'true'
+  })
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-bs-theme', darkMode ? 'dark' : 'light')
+    localStorage.setItem('darkMode', darkMode ? 'true' : 'false')
+  })
+
+  const handleToggle = () => {
+    setDarkMode((prevMode) => !prevMode)
+  }
+
   return (
     <Navbar expand="md" className="bg-body-tertiary" sticky="top">
       <Container>
@@ -12,6 +27,16 @@ function NavBar() {
             <Nav.Link href="/media">Media</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        <Form className="d-flex align-items-center">
+          <Form.Check 
+            type="switch"
+            id="dark-mode-switch"
+            label="Dark Mode"
+            checked={darkMode}
+            onChange={handleToggle}
+            className="ms-auto"
+          />
+        </Form>
       </Container>
     </Navbar>
   );
